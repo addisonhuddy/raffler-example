3da7326771680c84c22cc324347611d40888bdde
Addison Huddy <addisonhuddy@gmail.com>
2014-04-02 19:38:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raffler::Application.config.secret_key_base = '2bf032e8d437d2d1065be674457da8b4c2ada67570d5e9d3589edd01ff435023b4865cadb6c3745f7307acf9bfa9e7d282cefa19aa85fc7faaab2558e1575e8a'
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Entry.create!(name: "Addison")
+Entry.create!(name: "Gabi")
+Entry.create!(name: "Walker")
+Entry.create!(name: "Kevin")
